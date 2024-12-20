2841a66ea9c41eb36b6e24c0f0344580af6a2949
dhruvalgondaliya <f342dhruval@gmail.com>
2024-12-20 15:28:07 +0530

    add some changes

--- BHAGWATDHAM-CLIENT/client/src/components/ui/Button.jsx
+++ BHAGWATDHAM-CLIENT/client/src/components/ui/Button.jsx
@@ -1,9 +1,9 @@
+// class name button props transfor
 export function Button({ children, className, ...props }) {
     return (
         <button
             className={`px-4 py-2 rounded font-medium ${className}`}
-            {...props}
-        >
+            {...props}>
             {children}
         </button>
     );
